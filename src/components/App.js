c859736431169e669ab4efe3a5d48fbb9bf94d65
Matti Cone <mcone@uoregon.edu>
2018-04-27 13:41:00 -0700

    Initial commit, Includes react 16, webpack 4, Jest for tests

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return
+};
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+              <h1>Welcome to the Universal Wallet</h1>
+            </div>
+        );
+    }
+}

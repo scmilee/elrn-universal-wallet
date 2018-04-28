106ff95a76129a7aa55c0d7506fb9f89b71b7586
Cole Albon <calbon@cbtnuggets.com>
2018-04-28 10:03:52 -0700

    install react redux router

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import { render } from 'react-dom'
+import { connect, Provider } from 'react-redux'
 
-const Index = () => {
-  return
-};
+import { Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router'
 
+const ConnectedSwitch = connect(state => ({
+  location: state.location
+}))(Switch)
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-              <h1>Welcome to the Universal Wallet</h1>
-            </div>
-        );
-    }
-}
+const App = () => (
+  <ConnectedSwitch>
+    <Route exact path="/" component={() => (<h1>Home <Link to="/about">About</Link></h1>)} />
+    <Route path="/about" component={() => (<h1>About <Link to="/">Home</Link></h1>)} />
+  </ConnectedSwitch>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+
+const history = createHistory()
+const middleware = routerMiddleware(history)
+
+const store = createStore(
+  combineReducers({
+      router: routerReducer
+  }),
+  applyMiddleware(middleware, createLogger(), thunkMiddleware)
+)
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>,
     document.getElementById('app')
-
 );

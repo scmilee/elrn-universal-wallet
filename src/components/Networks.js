e4e4e0cbb533f5b7a846230db7a331d43567b5f5
Cole Albon <calbon@cbtnuggets.com>
2018-05-28 08:40:11 -0700

    add bip44 field

--- src/components/Networks.js
+++ src/components/Networks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { listNetworks } from '../actions/networkActions'
+const coininfo = require('coininfo')
 
 const mapStateToProps = ({networks}) => {
     console.log(networks);
@@ -16,7 +17,17 @@ const Networks = ({networks, ...rest}) => {
         <div>
             Networks.js
             <br></br>
-            { networks.map((network) => (<div>{network}<br></br></div>)) }
+            { networks.map((network) => ((
+                <div>
+                id: {network.id}&nbsp;
+                bip44: {network.bip44}&nbsp;
+                wif: {network.wif}&nbsp;
+                pubKeyHash: {network.pubKeyHash}&nbsp;
+                scriptHash: {network.scriptHash}&nbsp;
+                public: {network.bip32.public}&nbsp;
+                private: {network.bip32.private}&nbsp;
+                <br></br></div>))
+            )}
         </div>
     )
 }

473b253a57d5f3d4243ce5fccc51c6824fbf57f6
scmilee <drudged@oregonstate.edu>
2018-06-21 13:29:05 -0700

    add tests to App.test.js -- still need work

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,45 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import renderer from 'react-test-renderer';
+import TestUtils from 'react-dom/test-utils';
+import Elrn from '../elrn-config/elrn.js'
+import * as AuthPackage from './Authenticated'
+import Public from './Public'
+import * as AppPackage from './App'
+jest.mock('../elrn-config/elrn.js');
+jest.mock('./Public' );
+jest.mock('react-router-dom');
+jest.mock('./Authenticated');
+const auth= AuthPackage.Authenticated
+const App = AppPackage.App;
+
+const store = {
+	user: {
+		isAuthenticated: true
+	}
+}
+function appRender() {
+	const div = document.createElement('div');
+	const component = renderer.create(<App user={store.user} />, div);
+	return component;
+}
+
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+  	appRender();
+});
+
+it('renders Secret Component if user is authenticated', () => {
+	const wrapper = TestUtils.renderIntoDocument(
+		<App user={store.user}></App>
+	);
+	const realChild = TestUtils.findRenderedComponentWithType(wrapper, auth)
+	expect(realChild).toBeTruthy();
+	//expect(Authenticated).toHaveBeenCalled();
+	//expect()
+});
+
+it('does not render Secret Component if user is authenticated', () => {
+	const component = appRender();
+	//expect(Authenticated).toBeFalsy;
+});

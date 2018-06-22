27b5555bf53594ef1225b51d19e56221571429e5
scmilee <drudged@oregonstate.edu>
2018-06-22 08:50:16 -0700

    refactor App.test and add a helper function

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -18,26 +18,54 @@ const store = {
 		isAuthenticated: true
 	}
 }
+//checks the props of all children of app looking at the specific prop thats passed in 
+//and comparing values to the expected propvalue
+function childrenChecker(children, Prop, propValue) {
+		for (var i = 0; i < children.length; i++) {
+			if (children[i]._fiber.pendingProps[arguments[1]] === propValue) {
+				return true;
+			}
+		}
+		return false;
+}
 
 describe('App.js', function(){
-
+	//make sure it can be loaded in
 	it('renders without crashing', () => {
 	  	renderer.create(<App user={store.user} />);
 	});
-
 	//testing if the authenticated pathway is hit, by checking the 
 	//pending props of the first element on the page
 	it('renders Secret Component if user is authenticated', () => {
-		const root = renderer.create(<App user={store.user} />).root;
-		expect(root.children[0].children[0]._fiber.pendingProps.name).toEqual('auth-home');
+		const root = renderer.create(<App user={store.user} />).root.children[0];
+		let childMatch = childrenChecker(root.children, 'name', 'auth-home');
+		expect(childMatch).toBe(true);
 	});
-
 	//testing if the public pathway is hit, by checking the 
 	//pending props of the first element on the page
 	it('renders public component if the user is not authenticated', () => {
 		store.user.isAuthenticated = false;
-		const root = renderer.create(<App user={store.user} />).root;
-		expect(root.children[0].children[0]._fiber.pendingProps.name).toEqual('home');
+		const root = renderer.create(<App user={store.user} />).root.children[0];
+		let childMatch = childrenChecker(root.children, 'name', 'home');
+		expect(childMatch).toBe(true);
+	});
+	//check for a logout path
+	it('renders logout Component', () => {
+		const root = renderer.create(<App user={store.user} />).root.children[0];
+		let childMatch = childrenChecker(root.children, 'path', '/logout');
+		expect(childMatch).toBe(true);
+	});
+	//check for a login path
+	it('renders login Component', () => {
+		const root = renderer.create(<App user={store.user} />).root.children[0];
+		let childMatch = childrenChecker(root.children, 'path', '/login');
+		expect(childMatch).toBe(true);
+	});
+	//check for a handle-login path name
+	it('renders Logout Component', () => {
+		const root = renderer.create(<App user={store.user} />).root.children[0];
+		let childMatch = childrenChecker(root.children, 'name', 'handle-login');
+		expect(childMatch).toBe(true);
 	});
 
 });

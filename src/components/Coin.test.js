2b27ecb8dbed68f49b4999eb7a0f587f1ccbba4f
scmilee <drudged@oregonstate.edu>
2018-06-26 11:11:32 -0700

    add couple of tests surrounding Coin.js being able to mount, and calling its prop function when clicked

--- src/components/Coin.test.js
+++ src/components/Coin.test.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { shallow, mount, render } from 'enzyme'
+import Coin from './Coin.js'
+
+let mockFunc = jest.fn();
 
 describe('Coin.js', function(){
 	//make sure it can be loaded in
 	it('renders without crashing', () => {
-
+		const mountedCoin = shallow(<Coin onClick={mockFunc}/>);
 	});
 
-	it('doesn\'t call handleApproveShiftButton on its own', function() {
-		
+	it('calls its prop function when clicked', () => {	
+		const mountedCoin = mount(<Coin onClick={mockFunc}/>);
+		mountedCoin.find('button').simulate('click');
+		expect(mockFunc).toHaveBeenCalled();
 	});
+	
 });

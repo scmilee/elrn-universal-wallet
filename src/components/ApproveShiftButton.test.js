3669ee34acdea1d0ff52171960375c68b4255ee7
scmilee <drudged@oregonstate.edu>
2018-06-26 14:50:34 -0700

    add test to prop mapping for approve shift button

--- src/components/ApproveShiftButton.test.js
+++ src/components/ApproveShiftButton.test.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { shallow, mount, render } from 'enzyme'
 import * as ApproveShiftButtonPackage from './ApproveShiftButton.js'
+import configureMockStore from 'redux-mock-store';
 jest.mock('../elrn-config/elrn.js');
-jest.mock('../actions/shapeShiftActions');
 jest.mock('../styles.js');
 
+const mockStore = configureMockStore();
 const ShiftButton = ApproveShiftButtonPackage.ApproveShiftButton;
+//connected meaning redux dispatches are being tested
+const ConnectedShiftButton = ApproveShiftButtonPackage.default;
 
 describe('ApproveShiftButton.js', function(){
 	//make sure it can be loaded in
@@ -25,4 +28,35 @@ describe('ApproveShiftButton.js', function(){
 		const button = shallow(<ShiftButton handleApproveShiftButton={mockHandle} />);
 		expect(mockHandle).not.toHaveBeenCalled();
 	});
+});
+
+describe('ConnectedShiftButton', function(){ 
+	let wrapper, state, store;
+
+	beforeEach(() => {
+        state = {
+        	wallet:{
+        		address: 'avcds',
+        		returnAddress: 'avcs'
+        	},
+        	shapeShift:{
+        		toSymbol: 'btc',
+   				fromSymbol: 'cvc',
+   				shiftAmount: '2.5'
+        	}
+        };
+        store = mockStore(state);
+        wrapper = shallow(
+            <ConnectedShiftButton store={store} />
+        );
+    });
+
+	it('should correctly map the state to props', () => {
+		expect(wrapper.props().withdrawalAddress).toBe('avcds');
+		expect(wrapper.props().returnAddress).toBe('avcs');
+		expect(wrapper.props().toSymbol).toBe('btc');
+		expect(wrapper.props().fromSymbol).toBe('cvc');
+		expect(wrapper.props().shiftAmount).toBe('2.5')
+	});
+
 });

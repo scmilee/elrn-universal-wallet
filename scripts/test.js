c0c2e7999135ed6d6dcc94b5f325177c6f8cde87
scmilee <drudged@oregonstate.edu>
2018-06-27 11:08:18 -0700

    remove --watch push as it was causing errors

--- scripts/test.js
+++ scripts/test.js
@@ -13,12 +13,12 @@ process.on('unhandledRejection', err => {
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require('../config/env.js');
 const jest = require('jest');
 const argv = process.argv.slice(2);
 
 // // Watch unless on CI or in coverage mode
-// if (!process.env.CI && argv.indexOf('--coverage') < 0) {
+//  if (!process.env.CI && argv.indexOf('--coverage') < 0) {
 //   argv.push('--watch');
 // }
 

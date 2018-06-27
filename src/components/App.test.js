c008e5800d5683cfb33e63975a032c03c20bc58c
scmilee <drudged@oregonstate.edu>
2018-06-27 15:03:47 -0700

    refactor app.test.js to use enzyme instead of a function of my own creation. Shaves 1.5 seconds off test time

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import renderer from 'react-test-renderer';
-import TestUtils from 'react-dom/test-utils';
+import {shallow } from 'enzyme'
 import * as AppPackage from './App'
+
 jest.mock('../elrn-config/elrn.js');
-jest.mock('./Public' );
-jest.mock('react-router-dom');
-jest.mock('./Authenticated');
+
 
 //testing just the app, not the connections to redux as well
 const App = AppPackage.App;
@@ -15,63 +13,43 @@ const store = {
 		isAuthenticated: true
 	}
 }
-//checks the props of all children of app looking at the specific prop thats passed in 
-//and comparing values to the expected propvalue
-function childrenChecker(children, Prop, propValue) {
-	for (var i = 0; i < children.length; i++) {
-		if (children[i]._fiber.pendingProps[arguments[1]] === propValue) {
-			return true;
-		}
-	}
-	return false;
-}
-//preface these tests by saying that I couldn't get enzymes api to work with finding mocked
-//child components so i had to build by own child finder
 
 describe('App.js', function(){
 	//make sure it can be loaded in
-	let root;
+	let rootApp;
+
 	beforeEach(() => {
-		root = renderer.create(<App user={store.user} />).root.children[0];
+		rootApp = shallow(<App user={store.user} />);
 	});
 
 	it('renders without crashing', () => {
-	  	renderer.create(<App user={store.user} />);
+	  	rootApp;
 	});
-	//testing if the authenticated pathway is hit, by checking the 
-	//pending props of the first element on the page
+
 	it('renders Secret Component if user is authenticated', () => {	
-		let childMatch = childrenChecker(root.children, 'name', 'auth-home');
-		expect(childMatch).toBe(true);
+		expect(rootApp.find({name: 'auth-home'})).toHaveLength(1);	
 		//make sure the public home component isn't created
-		childMatch = childrenChecker(root.children, 'name', 'home');
-		expect(childMatch).toBe(false);
+		expect(rootApp.find({name: 'home'})).toHaveLength(0);
 	});
-	//testing if the public pathway is hit, by checking the 
-	//pending props of the first element on the page
+	
 	it('renders public component if the user is not authenticated', () => {
 		store.user.isAuthenticated = false;
-		root = renderer.create(<App user={store.user} />).root.children[0];
-		let childMatch = childrenChecker(root.children, 'name', 'home');
-		expect(childMatch).toBe(true);
-		//make sure authorized component isn't spawned
-		childMatch = childrenChecker(root.children, 'name', 'auth-home');
-		expect(childMatch).toBe(false);
+		rootApp = shallow(<App user={store.user} />);
+	
+		expect(rootApp.find({name: 'home'})).toHaveLength(1);
+		expect(rootApp.find({name: 'auth-home'})).toHaveLength(0);
 	});
 	//check for a logout path
 	it('renders logout Component', () => {
-		let childMatch = childrenChecker(root.children, 'path', '/logout');
-		expect(childMatch).toBe(true);
+		expect(rootApp.find({path: '/logout'})).toHaveLength(1);
 	});
 	//check for a login path
 	it('renders login Component', () => {
-		let childMatch = childrenChecker(root.children, 'path', '/login');
-		expect(childMatch).toBe(true);
+		expect(rootApp.find({path: '/login'})).toHaveLength(1);
 	});
 	//check for a handle-login path name
 	it('renders Logout Component', () => {
-		let childMatch = childrenChecker(root.children, 'name', 'handle-login');
-		expect(childMatch).toBe(true);
+		expect(rootApp.find({path: '/handle-login'})).toHaveLength(1);
 	});
 
 });

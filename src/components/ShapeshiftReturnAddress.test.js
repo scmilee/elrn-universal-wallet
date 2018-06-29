7acacca45e47021599d130322313c9e69a3130f7
scmilee <drudged@oregonstate.edu>
2018-06-29 10:50:16 -0700

    add tests surrounding the components ability to render, and that it porperly displays its prop return address

---
+++ src/components/ShapeshiftReturnAddress.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import * as ShapeshiftReturnAddressPackage from './ShapeshiftReturnAddress.js'
+
+jest.mock('../elrn-config/elrn.js');
+
+const ShapeshiftReturnAddress = ShapeshiftReturnAddressPackage.ShapeshiftReturnAddress;
+let mnemonicPayload = 'addressTest';
+
+describe('ShapeshiftReturnAddress.js', function(){
+	//make sure it can be loaded in
+	let mountedShapeshiftReturnAddress;
+
+	beforeEach(() => {
+		mountedShapeshiftReturnAddress = shallow(<ShapeshiftReturnAddress  returnAddress={mnemonicPayload}/>);
+	});
+	it('renders without crashing', () => {
+		mountedShapeshiftReturnAddress;
+	});
+
+	it('correctly displays its prop ShapeshiftReturnAddress', () => {
+		expect(mountedShapeshiftReturnAddress.text()).toEqual(mnemonicPayload);
+	});
+
+});
